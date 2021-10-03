2205a3615be906e8b3e70841f7848181c625754c
AndreyShef <nava2007@yandex.ru>
2021-10-03 15:37:23 +0300

    изменена логика поиска

--- blog/views.py
+++ blog/views.py
@@ -108,12 +108,12 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            # search_vector = SearchVector('title', 'body')
-            # search_query = SearchQuery(query)
-            # results = Post.objects.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)
-            #                                 ).filter(search=search_query).order_by('-rank')
-            results = Post.objects.annotate(similarity=TrigramSimilarity('title', query),
-                                            ).filter(similarity__gt=0.3).order_by('-similarity')
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.objects.annotate(search=search_vector, rank=SearchRank(search_vector, search_query)
+                                            ).filter(search=search_query).order_by('-rank')
+            # results = Post.objects.annotate(similarity=TrigramSimilarity('title', query),
+            #                                 ).filter(similarity__gt=0.3).order_by('-similarity')
     return render(request, 'blog/post/search.html', {'form': form,
                                                      'query': query,
                                                      'results': results})
